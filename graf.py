c361f8332e9decb2dfa3beb42eb1c83e1ad7864f
Dmitriy <cubarrv@gmail.com>
2024-09-24 14:18:05 +0300

    final

--- graf.py
+++ graf.py
@@ -2,24 +2,27 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from my import model, X_train, y_test, X_test
 
-from my import model, X_train, y_test, test
+def matrix():
+    test = model.predict(X_test)
+    cm = confusion_matrix(y_test,test)
+    plt.figure(figsize=(6,4))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False, xticklabels=['Остались', 'Ушли'], yticklabels=['Остались', 'Ушли'])
+    plt.title("Матрица ошибок")
+    plt.show()
 
-cm = confusion_matrix(y_test,test)
+def w():
+    importances = model.feature_importances_
+    indices = np.argsort(importances)[::-1]
+    plt.figure(figsize=(8,6))
+    plt.title("Важность признаков")
+    plt.bar(range(X_train.shape[1]), importances[indices], align="center")
+    plt.xticks(range(X_train.shape[1]), X_train.columns[indices], rotation=90)
+    plt.tight_layout()
+    plt.show()
 
-plt.figure(figsize=(6,4))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False, xticklabels=['Остались', 'Ушли'], yticklabels=['Остались', 'Ушли'])
-plt.title("Матрица ошибок")
-plt.show()
+print(matrix())
+print(w())
 
-# Получаем важность признаков
-importances = model.feature_importances_
-indices = np.argsort(importances)[::-1]
 
-# Строим график
-plt.figure(figsize=(8,6))
-plt.title("Важность признаков")
-plt.bar(range(X_train.shape[1]), importances[indices], align="center")
-plt.xticks(range(X_train.shape[1]), X_train.columns[indices], rotation=90)
-plt.tight_layout()
-plt.show()
--- my.py
+++ my.py
@@ -2,36 +2,35 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
+def one(df):
+    df['Услуга международных звонков'] = df['Услуга международных звонков'].apply(lambda x: 1 if x == "True" else 0)    # 1 - остался
+    df['Услуга VMail'] = df['Услуга VMail'].apply(lambda x: 1 if x == "True" else 0)    # 1 - остался
+    return df
+
+def main():
+    df = pd.read_csv('/Users/dmitrij/Desktop/Тестовое задание на позицию аналитика/churn.txt', encoding='windows-1251')
+    df = df.drop(columns=['Номер телефона', 'Штат'])
+    df = one(df)
+    df['Уход'] = df['Уход'].apply(lambda x: 0 if str(x) == 'True' else 1)    # 0 - остался  
+    return df
+
+def ml(df):
+    global model, X_train, X_test, y_train, y_test
+
+    X = df.drop(columns=['Уход'])
+    y = df['Уход']
+    X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=0.6, random_state=42)
+    model = RandomForestClassifier(n_estimators=150, random_state=42)
+    model.fit(X_train,y_train)
+    df_new = pd.read_csv('/Users/dmitrij/Desktop/Тестовое задание на позицию аналитика/new.txt', encoding='windows-1251')
+    df_new = one(df_new)
+    df_new = df_new.drop(columns=['Номер телефона', 'Штат','Уход'])
+    test_new = model.predict(df_new)
+    return test_new
+    
 
 
-df = pd.read_csv('/Users/dmitrij/Desktop/Тестовое задание на позицию аналитика/churn.txt', encoding='windows-1251')
-
-df = df.drop(columns=['Номер телефона', 'Штат'])
-
-df['Услуга международных звонков'] = df['Услуга международных звонков'].apply(lambda x: 1 if x == "True" else 0)    # 1 - остался
-df['Услуга VMail'] = df['Услуга VMail'].apply(lambda x: 1 if x == "True" else 0)    # 1 - остался
-df['Уход'] = df['Уход'].apply(lambda x: 0 if str(x) == 'True' else 1)    # 0 - остался
-
-X = df.drop(columns=['Уход'])
-y = df['Уход']
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=0.6, random_state=42)
-
-
-model = RandomForestClassifier(n_estimators=150, random_state=42)
-model.fit(X_train,y_train)
-
-test = model.predict(X_test)
-
-df_new = pd.read_csv('/Users/dmitrij/Desktop/Тестовое задание на позицию аналитика/new.txt', encoding='windows-1251')
-
-df_new['Услуга международных звонков'] = df_new['Услуга международных звонков'].apply(lambda x: 1 if x == "True" else 0)    # 1 - остался
-df_new['Услуга VMail'] = df_new['Услуга VMail'].apply(lambda x: 1 if x == "True" else 0)    # 1 - остался
-
-df_new = df_new.drop(columns=['Номер телефона', 'Штат','Уход'])
-
-test_new = model.predict(df_new)
-print(test_new)
+print(ml(main()))
 
 
 
